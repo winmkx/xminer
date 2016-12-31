caf22ec1f43c864cb4b45734b630ce0476260e35
WinMKX <winmkx@gmail.com>
2016-12-31 17:38:28 +1300

    updated version in Windows build

--- winbuild/dist/include/config.h
+++ winbuild/dist/include/config.h
@@ -67,11 +67,11 @@
 
 #endif
 
-#define VERSION "v5.2.0"
+#define VERSION "v5.5.4"
 #define PACKAGE_NAME "sgminer"
 #define PACKAGE_TARNAME "sgminer"
-#define PACKAGE_VERSION "5.2.0"
-#define PACKAGE_STRING "sgminer 5.2.0"
+#define PACKAGE_VERSION "5.5.4"
+#define PACKAGE_STRING "sgminer 5.5.4"
 #define PACKAGE "sgminer"
 
 #define SGMINER_PREFIX ""
